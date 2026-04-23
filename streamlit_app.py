6fb7e38ae7c4665e18161eb9e6f40bd3bb69481b
diegonavarrete1 <diegonavarreteruvalcaba@ciencias.unam.mx>
2026-04-23 13:04:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,22 +9,23 @@ st.set_page_config(
     page_icon='📉',
 )
 
-# Función para descargar datos
-
 @st.cache_data
 def get_data():
-    ticker = "AAPL"  # Activo AAPL
+    ticker = "AAPL"
     data = yf.download(ticker, start="2010-01-01")
 
-    # Calcular rendimientos
-    data['Returns'] = data['Adj Close'].pct_change()
+    # Ver qué columnas hay
+    if 'Adj Close' in data.columns:
+        data['Returns'] = data['Adj Close'].pct_change()
+    else:
+        data['Returns'] = data['Close'].pct_change()
+
     data = data.dropna()
 
     return data
 
 data = get_data()
 
-# -----------------------------------------------------------------------------
 # Página principal
 
 st.title("📉 Análisis de Riesgo Financiero")
@@ -40,8 +41,7 @@ st.header("Rendimientos diarios", divider="gray")
 
 st.line_chart(data['Returns'])
 
-# -----------------------------------------------------------------------------
-# Estadísticas básicas (inciso b)
+
 
 st.header("Estadísticas", divider="gray")
 
@@ -51,6 +51,6 @@ curtosis = data['Returns'].kurt()
 
 col1, col2, col3 = st.columns(3)
 
-col1.metric("Media", f"{mean:.6f}")
-col2.metric("Sesgo", f"{skew:.4f}")
-col3.metric("Curtosis", f"{kurt:.4f}")
+col1.metric("Media", f"{media:.6f}")
+col2.metric("Sesgo", f"{sesgo:.4f}")
+col3.metric("Curtosis", f"{curtosis:.4f}")
